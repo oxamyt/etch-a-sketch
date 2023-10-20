92d044955fcbd6f8b40d7077552128ff0200868c
Oxamyt <dentez.danil@gmail.com>
2023-10-20 22:58:47 +0300

    create-drawing-pad

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+// container
 const container = document.querySelector('#container');
 
+// grid function
 function createDivs(numDivs){
     for (let i = 0; i < numDivs; i++){
         let divs = document.createElement('div');
+        divs.classList.add('conts')
         container.appendChild(divs);
+        
     }
 }
 createDivs(256);
+
+// hovering event
+const divs = document.querySelectorAll('.conts');
+divs.forEach(div  => {
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'pink';
+    })
+    }
+)
+
+// button event
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    const ask = prompt('How big you want grid to be? (1-100)')
+})
+
