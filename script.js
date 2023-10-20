f7c2f7eff6ff7b6b599a2662aebf73c9854fed95
Oxamyt <dentez.danil@gmail.com>
2023-10-20 21:56:00 +0300

    create-grid-layout

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-let container = document.querySelector('.container');
+const container = document.querySelector('#container');
+
+function createDivs(numDivs){
+    for (let i = 0; i < numDivs; i++){
+        let divs = document.createElement('div');
+        container.appendChild(divs);
+    }
+}
+createDivs(256);
