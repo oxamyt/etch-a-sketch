e7017067932d58283350516a6297fff15510e694
Oxamyt <dentez.danil@gmail.com>
2023-10-21 23:01:17 +0300

    create-change-grid-possibility

--- script.js
+++ script.js
@@ -1,29 +1,60 @@
 // container
 const container = document.querySelector('#container');
 
-// grid function
-function createDivs(numDivs){
-    for (let i = 0; i < numDivs; i++){
-        let divs = document.createElement('div');
-        divs.classList.add('conts')
-        container.appendChild(divs);
-        
+// function to create divs
+function createDivs(numDivs) {
+    for (let i = 0; i < numDivs; i++) {
+        let div = document.createElement('div'); 
+        div.classList.add('conts');
+        container.appendChild(div);
     }
 }
-createDivs(256);
 
-// hovering event
-const divs = document.querySelectorAll('.conts');
-divs.forEach(div  => {
-    div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = 'pink';
-    })
-    }
-)
+// Hover event
+function addHoverEventListeners() {
+    const divs = document.querySelectorAll('.conts');
+    divs.forEach(div => {
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = 'pink';
+        });
+    });
+}
 
-// button event
-const button = document.querySelector('button');
+createDivs(100);
+addHoverEventListeners();
+
+// Remove divs function
+function removeDivs() {
+    const divs = document.querySelectorAll('.conts');
+    divs.forEach(div => {
+        container.removeChild(div);
+    });
+}
+
+// Button event
+const button = document.querySelector('#change');
 button.addEventListener('click', () => {
-    const ask = prompt('How big you want grid to be? (1-100)')
-})
+    const ask = prompt('How big do you want the grid to be? (1-100)');
+    if (ask < 100){
+        removeDivs();
+        createDivs(ask * ask);
+        addHoverEventListeners(); 
+        gridCalc(ask);
+    }
+    else  {
+        alert('Please, input 1-100')
+    }
+});
 
+function gridCalc(ask) {
+    const containerWidth = container.clientWidth;
+    const containerHeight = container.clientHeight;
+    let divheight = containerHeight / ask;
+    let divwidth = containerWidth / ask;
+
+    const divs = document.querySelectorAll('.conts'); 
+    divs.forEach(div => {
+        div.style.width = `${divwidth}px`;
+        div.style.height = `${divheight}px`;
+    });
+}
